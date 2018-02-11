b821c3184358c2e3ff86238ff23dbdb2cc05c852
Giuseppe de Marino <giuche@gmail.com>
2018-02-11 19:11:17 +0000

    forgot to add loader

---
+++ src/components/loader/Loader.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Loader = () => (
+  <div className="loader">
+    <p className="loader__text">Searching for your movies..</p>
+    <div className="loader__container"></div>
+  </div>
+);
+
+export default Loader;
